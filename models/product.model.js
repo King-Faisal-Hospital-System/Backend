a3968e0a565c1d6afb792f61e513bb76d4678a6e
Polo <148433691+MugishaProsper@users.noreply.github.com>
2025-08-21 10:15:53 +0200

    refined version

--- models/product.model.js
+++ models/product.model.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const productSchema = mongoose.Schema({
-    name : { type : String },
-    price : { type : Number },
-    category : { type : String, enum : ["TABLETS", "CAPSULE", "SYRUP", "INJECTION", "CREAM", "DROPS"], default : "LIQUID"},
-    form : { type : String, enum : ["PIECES", "BOXES", "BOTTLES", "PACKS", "KILOGRAMS", "LITERS"] },
-    batch : { type : String },
-    
-}, { timestamps : true });
+    name: { type: String },
+    price: { type: Number },
+    category: { type: String, enum: ["TABLETS", "CAPSULE", "SYRUP", "INJECTION", "CREAM", "DROPS"], default: "LIQUID" },
+    form: { type: String, enum: ["PIECES", "BOXES", "BOTTLES", "PACKS", "KILOGRAMS", "LITERS"] },
+    batch: { type: String },
+    quantity: { type: Number, default: 0 }
+}, { timestamps: true });
 
 const Product = mongoose.model("products", productSchema);
 export default Product;
--- models/user.model.js
+++ models/user.model.js
@@ -6,10 +6,8 @@ const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     phone_number : { type : String, required : true, unique : true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["ADMIN", "STOCK_MANAGER", "SUPPLIER"], default : "ADMIN" },
-    isVerified : { type : Boolean, default : false },
-    company_name : { type : String },
-    company_phone : { type : String }
+    role: { type: String, enum: ["ADMIN", "STOCK_MANAGER"], default : "ADMIN" },
+    isVerified : { type : Boolean, default : false }
 }, { timestamps: true });
 
 const User = mongoose.model("users", userSchema);
