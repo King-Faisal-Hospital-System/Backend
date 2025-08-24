bc9ee2bfb94aaffe22368d116346e29d15726924
Polo <148433691+MugishaProsper@users.noreply.github.com>
2025-08-24 17:54:55 +0200

    completed email-based purchase order requests

---
+++ services/cloudinary.services.js
@@ -0,0 +1,15 @@
+import cloudinary from "../config/cloudinary.config.js"
+
+export const uploadReportToCloudinary = (pdfBuffer, publicId="reports") => {
+    return new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream({
+            resource_type : "raw",
+            folder : "reports",
+            public_id : publicId
+        }, (error, result) => {
+            if(error) reject(error);
+            else resolve(result.secure_url);
+        });
+        stream.end(pdfBuffer)
+    })
+}
--- utils/file.conversion.utils.js
+++ utils/file.conversion.utils.js
@@ -1,10 +1,22 @@
 import puppeteer from "puppeteer";
-import fs from "fs";
 
-export async function convertHtmlToPdf(htmlContent, outputPath) {
+export async function convertHtmlToPdfBuffer(htmlContent) {
     try {
         const browser = puppeteer.launch();
-        const page = (await browser).newPage()
+        const page = (await browser).newPage();
+        (await page).setContent(htmlContent, { waitUntil : "networkidle0"});
+        const pdfBuffer = (await page).pdf({
+            format : "A4",
+            printBackground : true,
+            margin : {
+                top : "20px",
+                bottom : "20px",
+                left : "20px",
+                right : "20px"
+            }
+        });
+        (await browser).close();
+        return pdfBuffer
     } catch (error) {
         throw new Error(error)
     }        
