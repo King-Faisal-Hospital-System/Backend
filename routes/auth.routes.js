29bbb7454af95294972d5c90e36aa773a96543ae
IRANZI <iradianah5@gmail.com>
2025-08-27 15:09:16 +0200

    Sign In Fixes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { login, logout, register } from "../controllers/auth.controllers.js";
+import { login, logout, register,getMe } from "../controllers/auth.controllers.js";
 import { authorize } from "../middlewares/auth.middleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.get("/me", authorize, getMe);
 authRouter.post("/logout", authorize, logout);
 
 export default authRouter
