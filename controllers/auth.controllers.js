58abdee37d3a9d5a94559168b5f84ce8765d5d97
IRANZI <iradianah5@gmail.com>
2025-08-27 11:06:35 +0200

    Login adjustments

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -56,37 +56,26 @@ export const register = async (req, res) => {
 // LOGIN
 export const login = async (req, res) => {
   const { email, password, role } = req.body;
-
-  if (!email || !password || !role) {
-    return res.status(400).json({ message: "Email, password, and role are required" });
-  }
+  if (!role) return res.status(404).json({ message: "Role not found" });
 
   try {
-    
-    const user = await User.findOne({ email, role: role.toUpperCase() });
+    const user = await User.findOne({ email, role });
     if (!user) return res.status(404).json({ message: "User not found" });
 
-    
     const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) return res.status(401).json({ message: "Incorrect password" });
+    if (!isPasswordValid)
+      return res.status(403).json({ message: "Incorrect password" });
 
-    
-    if (!user.isVerified) return res.status(403).json({ message: "User not verified by admin" });
+    if (!user.isVerified)
+      return res
+        .status(403)
+        .json({ message: "User not verified by admin" });
 
-   
     generateTokenAndSetCookie(user, res);
 
-    
-    return res.status(200).json({
-      message: "Login successful",
-      user: {
-        email: user.email,
-        role: user.role,
-        fullname: user.fullname,
-      },
-    });
+    return res.status(200).json({ message: "Login successful" });
   } catch (error) {
-    console.error("Login error:", error);
+    console.log(error);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
