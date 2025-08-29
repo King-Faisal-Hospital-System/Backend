746c685b0f0df3a7c3c59286101ac955f313d3f7
IRANZI <iradianah5@gmail.com>
2025-08-29 11:16:53 +0200

    user approving

--- models/settings.model.js
+++ models/settings.model.js
@@ -5,7 +5,7 @@ const settingsSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
-    unique: true
+    unique: true // unique index ensures one settings document per user
   },
   personalInfo: {
     name: {
