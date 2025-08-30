c93fbd396d43663659f79bf42362adf4505d1d4d
Polo <148433691+MugishaProsper@users.noreply.github.com>
2025-08-30 11:15:24 +0200

    added script to permit chrome binary

--- utils/file.conversion.utils.js
+++ utils/file.conversion.utils.js
@@ -1,43 +1,32 @@
 import puppeteer from "puppeteer";
 
-const convertHtmlToPdfBuffer = async (htmlContent) => {
-    let browser;
-    try {
-        console.log("Using executable : ", puppeteer.executablePath())
-        browser = await puppeteer.launch({
-            // headless: 'new',
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-gpu'
-            ],
-            executablePath : puppeteer.executablePath()
-        });
-        
-        const page = await browser.newPage();
-        await page.setContent(htmlContent, { waitUntil: "networkidle0" });
-        
-        const pdfBuffer = await page.pdf({
-            format: "A4",
-            printBackground: true,
-            margin: {
-                top: "20px",
-                bottom: "20px",
-                left: "20px",
-                right: "20px"
-            }
-        });
-        
-        return pdfBuffer;
-    } catch (error) {
-        console.error('PDF generation error:', error);
-        throw new Error(`PDF generation failed: ${error.message}`);
-    } finally {
-        if (browser) {
-            await browser.close();
-        }
-    }
-};
+export const convertHtmlToPdfBuffer = async (htmlContent) => {
+  let browser;
+  try {
+    const chromePath = puppeteer.executablePath();
 
-export default convertHtmlToPdfBuffer
+    console.log("Resolved Chrome path:", chromePath);
+
+    browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+      ],
+      executablePath: chromePath, // important
+    });
+
+    const page = await browser.newPage();
+    await page.setContent(htmlContent, { waitUntil: "networkidle0" });
+
+    return await page.pdf({
+      format: "A4",
+      printBackground: true,
+      margin: { top: "20px", bottom: "20px", left: "20px", right: "20px" },
+    });
+  } finally {
+    if (browser) await browser.close();
+  }
+};
